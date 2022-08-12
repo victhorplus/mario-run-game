00b74ae49db3b5cc727548c2237c4ab6647681af
victhorplus <victhor.souza@picpay.com>
2022-08-12 19:25:47 -0300

    feat: Add Mario with jump animation

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-console.log("oi")
+document.addEventListener("keydown", keyEvents);
+
+function keyEvents(event){
+    switch(event.code.toLowerCase()){
+        case 'space':
+            jumpEvent();
+            break;
+    }
+}
+
+function jumpEvent(){
+    let mario = document.querySelector(".mario");
+    mario.classList.add("mario--jump");
+    clearClass(mario, "mario--jump", 700)
+}
+
+function clearClass(object, classe, timeOut){
+    setTimeout(() => {
+        object.classList.remove(classe)
+    }, timeOut)
+}
